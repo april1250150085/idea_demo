7cb9185dcdb04c6fca116baab285865f0763c9c5
Hui Jingle <1250150085@qq.com>
2021-02-05 23:10:01 +0800

    calculator 2.0

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public int compute(int num1, int num2, char symbol) {
+    public int compute(int num1, int num2, String symbol) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
         Computable compute = ComputeFactory.getCompute(symbol);
         return compute.compute(num1, num2);
 
